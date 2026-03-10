a811fc60be1c8f7faac7e4a09f6241225779facf
irenesobron <147519094+irenesobron@users.noreply.github.com>
2026-03-10 21:25:34 +0100

    Update and rename Test_ToDo.java to Test_actualizacion_clase_early.java

---
+++ src/test/java/com/tt1/test/Test_actualizacion_clase_early.java
@@ -0,0 +1,14 @@
+package com.tt1.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+
+class Test_actualizacion_clase_early {
+
+    @Test
+    void testActualizarEarly(){
+        assertEquals(1,1);
+    }
+}
